8440cfde829439490c272507367182e4c6764607
Ruslan Lukoyanov <ruslan.ru226@mail.ru>
2025-04-12 21:28:40 +0400

    main: init project

---
+++ cmd/web/main.go
@@ -0,0 +1,13 @@
+package main
+
+const webPort = "80"
+
+func main() {
+	// init db
+	// create sessions
+	// init chanels
+	// init wg
+	// init cfg
+	// set up sending emails
+	// listen for web conns
+}
