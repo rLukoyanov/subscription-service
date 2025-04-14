ed231feaa11582d53b134279414511ade01c6030
Ruslan Lukoyanov <ruslan.ru226@mail.ru>
2025-04-14 21:14:20 +0400

    main: get info for plan

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 	"subscription-service/data"
 )
 
@@ -143,12 +144,6 @@ func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) ChooseSubscription(w http.ResponseWriter, r *http.Request) {
-	if !app.Session.Exists(r.Context(), "userID") {
-		app.Session.Put(r.Context(), "warning", "u must be logged in")
-		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
-		return
-	}
-
 	plans, err := app.Models.Plan.GetAll()
 	if err != nil {
 		app.ErrorLog.Panicln(err)
@@ -163,5 +158,20 @@ func (app *Config) ChooseSubscription(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
 
+	planID, _ := strconv.Atoi(id)
+	plan, err := app.Models.Plan.GetOne(planID)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "unable to find plan")
+		http.Redirect(w, r, "/members/plans", http.StatusSeeOther)
+		return
+	}
+
+	user, ok := app.Session.Get(r.Context(), "user").(data.User)
+	if !ok {
+		app.Session.Put(r.Context(), "error", "login first")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
 }
